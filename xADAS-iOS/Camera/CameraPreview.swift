50ec79ae4c8531e952640046544c4dd09cdee678
chuongnguyen89dn-ui <chuongnguyen89dn@gmail.com>
2026-09-18 22:29:50 +0700

    camera: revert broken aspect-fit preview sizing

--- xADAS-iOS/Camera/CameraPreview.swift
+++ xADAS-iOS/Camera/CameraPreview.swift
@@ -24,7 +24,7 @@ final class PreviewView: UIView {
     var videoPreviewLayer: AVCaptureVideoPreviewLayer { layer as! AVCaptureVideoPreviewLayer }
 
     func attach(session: AVCaptureSession) {
-        videoPreviewLayer.videoGravity = .resizeAspect
+        videoPreviewLayer.videoGravity = .resizeAspectFill
         guard attachedSession !== session else { return }
         attachedSession = session
         videoPreviewLayer.session = session
