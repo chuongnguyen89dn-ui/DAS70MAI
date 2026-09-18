0665e7f445d1f53a3f406ce37eee4c7106c4312c
chuongnguyen89dn-ui <chuongnguyen89dn@gmail.com>
2026-09-18 19:43:37 +0700

    camera: make iPhone capture portrait while preview follows capture

--- xADAS-iOS/Camera/CameraManager.swift
+++ xADAS-iOS/Camera/CameraManager.swift
@@ -106,7 +106,7 @@ final class CameraManager: NSObject, ObservableObject {
 
         if let connection = videoOutput.connection(with: .video) {
             if #available(iOS 17.0, *) {
-                if connection.isVideoRotationAngleSupported(0) { connection.videoRotationAngle = 0 }
+                if connection.isVideoRotationAngleSupported(90) { connection.videoRotationAngle = 90 }
             } else if connection.isVideoOrientationSupported {
                 connection.videoOrientation = .portrait
             }
